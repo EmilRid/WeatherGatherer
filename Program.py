fa958415319e5cdf574fbd8765df776523e59381
Emil <emilridulfo@gmail.com>
2019-10-16 21:53:36 +0200

    Added unit converter

--- Program.py
+++ Program.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-weatherURL = "https://weather.com/weather/today/l/8b2acedf23be2811693b172400157817be1e2c7b9962e5db45ee1a5f94a59a9c" #weather.com search result 
-weatherPage = requests.get(weatherURL)
+unit = "" #unit of measurement 
+weatherURL = "" #weather.com search result 
+weatherPage = requests.get(weatherURL) 
 weatherSoup = BeautifulSoup(weatherPage.text, "html.parser")
+farhenheit = int(weatherSoup.find(class_="today_nowcard-temp").get_text()[0:-1])
 
-print(weatherSoup.find(class_="today_nowcard-temp").get_text())
+if unit == "celsius":
+    celsius = (farhenheit-32)*5/9
+    print(round(celsius),"C")
+
+elif unit == "fahrenheit":
+    print(farhenheit, "F")
+
+else: 
+    print("Check unit for accurate spelling")
